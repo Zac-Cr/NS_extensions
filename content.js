2953b92bf348819534071b05043048938d15acf0
the1stof300 <zcrock300@gmail.com>
2020-10-27 14:05:04 -0600

    fixed ID Call MIsspell

--- content.js
+++ content.js
@@ -5,7 +5,7 @@ function onload(){
     var category = document.getElementById("input_custbody_bc_websitecategory2");
     var source = document.getElementById("inpt_custbody_bc_ordersource3");
     if(category!="Motosport"){
-        document.getElementById("input_custbody_bc_websitecategory2").value="MotoSport";
+        document.getElementById("inpt_custbody_bc_websitecategory2").value="MotoSport";
         document.getElementById("inpt_custbody_bc_ordersource3").value="GHO";
         document.getElementById("custbody_bc_authorizationamount_formattedValue").value="1.00";
         document.getElementById("getauth_fs").className="checkbox_ck";
