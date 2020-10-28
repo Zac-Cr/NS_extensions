c5b9886edc8c8e3779d9caf38851e47b7d58b8a0
the1stof300 <zcrock300@gmail.com>
2020-10-28 13:46:47 -0600

    added null verification

--- content.js
+++ content.js
@@ -6,11 +6,24 @@ document.addEventListener("click", onload);
 // });
 function onload(){
     
-    var category = document.getElementById("input_custbody_bc_websitecategory2");
+    var category = document.getElementById("input_custbody_bc_websitecategory2").value;
+    var changedCategory = "MotoSport";
+    var source = document.getElementById("indx_custbody_bc_websitecategory2").value;
+    var changeSource = "3";
     if(category!="Motosport"){
-        document.getElementById("indx_custbody_bc_websitecategory2").value="3";
-        document.getElementById("inpt_custbody_bc_websitecategory2").value="MotoSport";
-        document.getElementById("custbody_bc_websitecategory").value="3";
+        if(category){
+            category = changedCategory;
+        }
+        else{
+            category ="MotoSport";
+        }
+        if(source){
+            source = changeSource;
+        }
+        else{
+            source = "3";
+        }
+             document.getElementById("custbody_bc_websitecategory").value="3";
         // document.getElementById("indx_custbody_bc_ordersource3").value="5";
         // document.getElementById("custbody_bc_authorizationamount_formattedValue").value="1.00";
         // document.getElementById("getauth_fs").className="checkbox_ck";
