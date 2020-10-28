f680ee3ce3381831bcd2964b5b0739d27be85aa6
the1stof300 <zcrock300@gmail.com>
2020-10-28 11:58:31 -0600

    added three step changes

--- content.js
+++ content.js
@@ -9,9 +9,11 @@ function onload(){
     var category = document.getElementById("input_custbody_bc_websitecategory2");
     if(category!="Motosport"){
         document.getElementById("indx_custbody_bc_websitecategory2").value="3";
-        document.getElementById("indx_custbody_bc_ordersource3").value="5";
-        document.getElementById("custbody_bc_authorizationamount_formattedValue").value="1.00";
-        document.getElementById("getauth_fs").className="checkbox_ck";
+        document.getElementById("inpt_custbody_bc_websitecategory2").value="MotoSport";
+        document.getElementById("custbody_bc_websitecategory").value="3";
+        // document.getElementById("indx_custbody_bc_ordersource3").value="5";
+        // document.getElementById("custbody_bc_authorizationamount_formattedValue").value="1.00";
+        // document.getElementById("getauth_fs").className="checkbox_ck";
     }
     console.log("function onload call")
 }
