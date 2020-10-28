54fabe1f8bbebcc973825fe49bfb656a2ef9b899
the1stof300 <zcrock300@gmail.com>
2020-10-28 14:09:34 -0600

    bug

--- content.js
+++ content.js
@@ -9,21 +9,25 @@ function onload(){
     var category = document.getElementById("inpt_custbody_bc_websitecategory2").value;
     var changedCategory = "MotoSport";
     var source = document.getElementById("indx_custbody_bc_websitecategory2").value;
+    // document.getElementById("custbody_bc_websitecategory").value="3";
     var changeSource = "3";
     if(category!="Motosport"){
         if(category){
             category = changedCategory;
+            console.log("category null")
         }
         else{
             category ="MotoSport";
         }
         if(source){
             source = changeSource;
+            console.log("source null")
         }
         else{
             source = "3";
         }
-             document.getElementById("custbody_bc_websitecategory").value="3";
+        if()
+            
         // document.getElementById("indx_custbody_bc_ordersource3").value="5";
         // document.getElementById("custbody_bc_authorizationamount_formattedValue").value="1.00";
         // document.getElementById("getauth_fs").className="checkbox_ck";
