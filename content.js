e4197fe083f700384e5d7f979c26ac77ad275423
the1stof300 <zcrock300@gmail.com>
2020-10-28 08:52:21 -0600

    Changed ID call and Checkbox call

--- content.js
+++ content.js
@@ -1,14 +1,14 @@
-window.addEventListener("load", onload);
+windowa.addEventListener("load", onload);
 
 function onload(){
     
     var category = document.getElementById("input_custbody_bc_websitecategory2");
     var source = document.getElementById("inpt_custbody_bc_ordersource3");
     if(category!="Motosport"){
-        document.getElementById("inpt_custbody_bc_websitecategory2").value="MotoSport";
-        document.getElementById("inpt_custbody_bc_ordersource3").value="GHO";
+        document.getElementById("indx_custbody_bc_websitecategory2").value="3";
+        document.getElementById("indx_custbody_bc_ordersource3").value="5";
         document.getElementById("custbody_bc_authorizationamount_formattedValue").value="1.00";
-        document.getElementsByClassName("getauth_fs").className="checkbox_ck";
+        document.getElementById("getauth_fs").className="checkbox_ck";
     }
 }
 
