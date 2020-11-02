8fe85a3a193a4fa59e796a87f2c97095f06c0bd1
the1stof300 <zcrock300@gmail.com>
2020-11-02 15:16:12 -0700

    added auth and check

--- content.js
+++ content.js
@@ -1,16 +1,21 @@
 // window.addEventListener("load", onload);
 // document.addEventListener("click", onload); 
 
-setTimeout(SetCatnSource(),10000);
+setTimeout(SetCatnSource(),15000);
 
 function SetCatnSource(){
     
+    // Set catalog
     document.getElementById("inpt_custbody_bc_websitecategory2").value="MotoSport";
     document.getElementById("hddn_custbody_bc_websitecategory2").value="3";
     document.getElementById("indx_custbody_bc_websitecategory2").value="3";
+    // set ORder Source
     document.getElementById("inpt_custbody_bc_ordersource3").value="GHO";
     document.getElementById("hddn_custbody_bc_ordersource3").value="5";
     document.getElementById("indx_custbody_bc_ordersource3").value="5";
+    // Set auth amount
+    document.getElementById("custbody_bc_authorizationamount_formattedValue").value="1.00";
+    document.getElementsByClassName("checkbox_unck").className="checkbox_ck";
     console.log("function onload call")
 };
 
