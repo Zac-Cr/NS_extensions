a06e9e8a55087424de6c7580d3cc1437b497cf78
the1stof300 <zcrock300@gmail.com>
2020-11-02 13:31:03 -0700

    ordersource

--- content.js
+++ content.js
@@ -5,6 +5,9 @@ function onload(){
     document.getElementById("inpt_custbody_bc_websitecategory2").value="MotoSport";
     document.getElementById("hddn_custbody_bc_websitecategory2").value="3";
     document.getElementById("indx_custbody_bc_websitecategory2").value="3";
+    document.getElementById("inpt_custbody_bc_ordersource3").value="GHO";
+    document.getElementById("hddn_custbody_bc_ordersource3").value="5";
+    document.getElementById("indx_custbody_bc_ordersource3").value="5";
     console.log("function onload call")
 };
 
