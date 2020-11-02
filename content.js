1ade46f79684f5ff21e16e3005f2aa244e32498e
the1stof300 <zcrock300@gmail.com>
2020-11-02 12:51:02 -0700

    simple code

--- content.js
+++ content.js
@@ -1,36 +1,9 @@
 // window.addEventListener("load", onload);
-document.addEventListener("click", onload);
-// chrome.runtime.onMessage.addListener(function(response,sender,sendResponse){
-//     alert(response);
-// });
+document.addEventListener("click", onload); 
 function onload(){
     
-    var category = document.getElementById("inpt_custbody_bc_websitecategory2").value;
-    var changedCategory = "MotoSport";
-    var source = document.getElementById("indx_custbody_bc_websitecategory2").value;
-    // document.getElementById("custbody_bc_websitecategory").value="3";
-    var changeSource = "3";
-    if(category!="Motosport"){
-        if(category){
-            category = changedCategory;
-            console.log("category null")
-        }
-        else{
-            category ="MotoSport";
-        }
-        if(source){
-            source = changeSource;
-            console.log("source null")
-        }
-        else{
-            source = "3";
-        }
-            
-        // document.getElementById("indx_custbody_bc_ordersource3").value="5";
-        // document.getElementById("custbody_bc_authorizationamount_formattedValue").value="1.00";
-        // document.getElementById("getauth_fs").className="checkbox_ck";
-    }
+    document.getElementById("inpt_custbody_bc_websitecategory2").value="MotoSport";
     console.log("function onload call")
-}
+};
 
 console.log("Content script JS call")
