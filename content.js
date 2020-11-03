5d1ef13fc6df1e5e467a94d35b076058ed9c77b1
the1stof300 <zcrock300@gmail.com>
2020-11-03 09:54:14 -0700

    initial check

--- content.js
+++ content.js
@@ -4,6 +4,9 @@
 setTimeout(SetCatnSource(),15000);
 
 function SetCatnSource(){
+
+    // checks on edit or not
+    console.log(document.getElementById("inpt_custbody_bc_websitecategory2").value);
     
     // Set catalog
     document.getElementById("inpt_custbody_bc_websitecategory2").value="MotoSport";
@@ -18,10 +21,14 @@ function SetCatnSource(){
     //get auth check
     document.getElementById("getauth_fs_inp").checked=true;
     document.getElementById("getauth_fs").className="checkbox_ck checkboximage";
-    // set shipping method to standard free
+    // set shipping method to standard
     document.getElementById("inpt_shipmethod11").value="4. Standard Delivery (Includes UPS MI)";
     document.getElementById("hddn_shipmethod11").value="6087332";
     document.getElementById("indx_shipmethod11").value="4";
+    // set amount to free
+    document.getElementById("shippingcost_formattedValue").value="0.00";
+    document.getElementById("shippingcost").value="0.00";
+
     console.log("function onload call")
 };
 
