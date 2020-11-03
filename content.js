b9dca164104926ca932d8963fc2c11b3754764dc
the1stof300 <zcrock300@gmail.com>
2020-11-03 09:44:06 -0700

    shipping methods

--- content.js
+++ content.js
@@ -15,9 +15,13 @@ function SetCatnSource(){
     document.getElementById("indx_custbody_bc_ordersource3").value="5";
     // Set auth amount
     document.getElementById("custbody_bc_authorizationamount_formattedValue").value="1.00";
-    // not working yet
+    //get auth check
     document.getElementById("getauth_fs_inp").checked=true;
     document.getElementById("getauth_fs").className="checkbox_ck checkboximage";
+    // set shipping method to standard free
+    document.getElementById("inpt_shipmethod11").value="4. Standard Delivery (Includes UPS MI)";
+    document.getElementById("hddn_shipmethod11").value="6087332";
+    document.getElementById("indx_shipmethod11").value="4";
     console.log("function onload call")
 };
 
