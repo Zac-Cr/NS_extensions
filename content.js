306dedd8673c8baef844efecd3bb0bac57f18fa7
the1stof300 <zcrock300@gmail.com>
2020-11-23 15:30:22 -0700

    added email canned

--- content.js
+++ content.js
@@ -1,8 +1,6 @@
-setTimeout(OrderNotes(),5000);
-setTimeout(SetCatnSource(),15000);
-
-
-
+var selection = "";
+var firstinpt= "";
+var secondinpt= "";
 // Set Catelog to Motosport, Source to GHO, Shipping to Standard, 
 // Fee to 0, Auth amount to 1, and check the get auth button
 function SetCatnSource(){
@@ -37,7 +35,6 @@ function SetCatnSource(){
     catch{
     };
 };
-
 function OrderNotes(){
     try{
 
@@ -52,3 +49,95 @@ function OrderNotes(){
     }
 
 };
+
+
+setTimeout(OrderNotes(),5000);
+document.addEventListener("DOMContentLoaded",SetCatnSource);
+
+var receivedArray =[];
+
+chrome.runtime.onMessage.addListener(
+    function(message,sender,sendresponse){
+        if(message == "getmessage"){
+            getmessage();
+        }
+
+        console.log("message recieved");
+        receivedArray.push(message); 
+        console.log(receivedArray);   
+    }
+);
+
+function getmessage(){
+    selection = receivedArray[0];
+    switch(receivedArray.length){
+        case 2:
+            firstinpt = receivedArray[1];
+            break;
+        case 3:
+            firstinpt = receivedArray[1];
+            secondinpt = receivedArray[2];
+            break;
+    };
+    for(i=0;Object.keys(messages).length; i++){
+        if(selection == Object.keys(messages)[i]){
+        // find the classname or id tag for the email 
+        document.getElementsByClassName("public-DraftStyleDefault-block").innerHTML = messages[selection]
+        };
+        
+    };
+};
+
+var messages={
+    "rma_free" : `Hey!/n
+    /n
+    Return Number (RMA):${firstinpt}/n
+    /n
+    Label:${secondinpt}/n
+    /n
+    To return your order using our label, please write your RMA number on the outside of the package, print our UPS Return Label and drop your package at a UPS Facility or with a UPS Driver.
+    Please let us know if you are having any problems viewing this label, we can resend it in a different format. If you cannot print your label, either bring up your label on a smartphone for the UPS store to scan and print there, or forward this email directly to your UPS store./n
+    /n
+    Attention:/n
+    * Our labels only work with UPS/n
+    * This is a free and complimentary label/n
+    * Returns typically take 1-5 business days to process once they arrive back to us, although they can take longer during peak times/n
+    * Once your return has been processed, we will send you an email letting you know/n
+    /n    
+    The label has a tracking number included on it. Please note down this tracking number to keep track of your packages status!/n
+    /n
+    Thank you,
+    `,
+    "rma_cust" : `Hey!/n
+    /n
+    As stated in our return policy we do not pay for returns of an oversized item. The item you are trying to return is classified as oversized. If you would still like to return your product please get it shipped to/n
+    295 Technology Drive/n
+    Christiansburg, VA 24073-7383/n
+    /n
+    After we receive your product a full refund will be issued to your account. Make sure to put the RMA number on the label as well, your label should look like this/n
+    ATTN RMA: ${firstinpt}/n
+    295 Technology Drive/n
+    Christiansburg, VA 24073-7383/n`,
+    "rma_stnd" : `Hey!/n
+    /n
+    Return Number (RMA):${firstinpt}/n
+    /n
+    Label:${secondinpt}/n
+    /n
+    To return your order using our label, please write your RMA number on the outside of the package, print our UPS Return Label and drop your package at a UPS Facility or with a UPS Driver.
+    Please let us know if you are having any problems viewing this label, we can resend it in a different format. If you cannot print your label, either bring up your label on a smartphone for the UPS store to scan and print there, or forward this email directly to your UPS store.
+    /n
+    Attention:/n
+    * Our labels only work with UPS/n
+    *  The included label is a charged $6.99 label. That total, plus any applicable tax, will be taken out of your total refund/n
+    * Returns typically take 1-5 business days to process once they arrive back to us, although they can take longer during peak times/n
+    * Once your return has been processed, we will send you an email letting you know/n
+    /n
+    The label has a tracking number included on it. Please note down this tracking number to keep track of your packages status!/n
+    /n
+    Thank you,/n`,
+    "backorder" : `The items that are left are on a backorder. The estimated date of arrival from ${secondinpt} is ${firstinpt}. `,
+    "aut" : `Sorry that you seem to have 2 charges on your account. The first charge that you see that is a few cents more then the total amount was the authorization made to make sure the card was good. Due to legal reasons we cannot take the money out of the account until the product is leaving our warehouse on its way to you. Because of this, you will see a second charge that will match the original total of the order. The authorization that was placed when the order was submitted will then drop off your account within a couple days of the items being shipped. If you order was sent in seperate shipments you will see a charge equal to the total of the items of that shipment.`,
+}
+console.log(Object.keys(messages)[0])
+console.log(messages['test'])
---
+++ popup.js
@@ -0,0 +1,74 @@
+var messageArray=[];
+
+function changeinput(){ 
+  var rmainpt = document.getElementById("rma");
+  var labelinpt = document.getElementById('label');
+  var backinpt = document.getElementById('bckorder');
+  var vendorinpt = document.getElementById('vendor');
+  document.getElementById('submit').className='hddn';
+  rmainpt.className="hddn";
+  labelinpt.className='hddn';
+  backinpt.className="hddn";
+  vendorinpt.className="hddn";
+    switch(ext.value){
+      case "rma_free":
+        rmainpt.className ="unhidden";
+        labelinpt.className ="unhidden";
+        break;
+      case "auth":
+        break;
+      case 'rma_cust':
+        rmainpt.className ="unhidden";
+        break;
+      case 'rma_stnd':
+      rmainpt.className ="unhidden";
+      labelinpt.className ="unhidden";
+      break;
+      case 'backorder':
+        backinpt.className="unhidden";
+        vendorinpt.className="unhidden";
+        break;
+      case '':
+        document.getElementById('submit').className='hddn';
+        break;
+      }
+  document.getElementById('submit').className='unhidden';
+};
+  // var params ={
+  //   active: true,
+  //   currentWindow: true
+  // }
+
+  // chrome.tabs.query(params,getinputval); 
+
+function getinputval(){
+  messageArray.push(ext.value);
+  var length = inpt.length;
+   for(i=0; i < length; i++){
+    if(inpt[i].value != ""){
+      messageArray.push(inpt[i].value);
+      console.log("chromemessage if statement")
+    };
+  };
+  chromemessage();
+};
+
+function chromemessage(){
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    messageArray.forEach(element => {
+      chrome.tabs.sendMessage(tabs[0].id,element);
+    });
+    chrome.tabs.sendMessage(tabs[0].id,"getmessage");
+  });
+  console.log("chromemessage called")
+};
+
+
+var ext= document.getElementById("rspns_list");
+ext.addEventListener("change",changeinput);
+var inpt = document.getElementsByTagName('input');
+document.getElementById('submit').addEventListener('click', getinputval);
+
+
+// sending array to chrom
+// https://stackoverflow.com/questions/56532745/how-do-i-send-an-array-from-a-content-script-to-a-popup-script
