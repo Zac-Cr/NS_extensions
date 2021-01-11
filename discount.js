0cc0b1915e8ac197e99d0965fc965bd2e84fb4a6
the1stof300 <zcrock300@gmail.com>
2021-01-11 09:32:38 -0700

    try catch

--- discount.js
+++ discount.js
@@ -1,21 +1,24 @@
 function insertdscnt(text, dscnt){
     var selection = document.getElementsByClassName("uir-machine-row");
     for(i=0; i = selection.length; i++){
-        if(selection[i].id.startsWith("item_row")){
-            var index = selection[i].id
-            if(index[index.length - 1]%2 == 0){
-                selection[i].className = "uir-machine-row-focused listfocusedrow";
-                selection[i].children[2].click();
-                document.getElementById("item_insert").click();
-                var dscnttype = document.getElementById("item_item_display")
-                dscnttype.click();
-                dscnttype.value = text;
-                document.getElementById("inpt_price6").value = "Custom";
-                document.getElementById("rate_formattedValue").click();
-                document.getElementById("rate_formattedValue").value = dscnt;
-            }
-        };
-    };   
+        try{
+            if(selection[i].id.startsWith("item_row")){
+                var index = selection[i].id
+                if(index[index.length - 1]%2 == 0){
+                    selection[i].className = "uir-machine-row-focused listfocusedrow";
+                    selection[i].children[2].click();
+                    document.getElementById("item_insert").click();
+                    var dscnttype = document.getElementById("item_item_display")
+                    dscnttype.click();
+                    dscnttype.value = text;
+                    document.getElementById("inpt_price6").value = "Custom";
+                    document.getElementById("rate_formattedValue").click();
+                    document.getElementById("rate_formattedValue").value = dscnt;
+                }
+            };
+        }
+        catch{};   
+    };
 
 }
 
